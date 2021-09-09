e57561875736ef9093364bf6726fec49872c6ae5
Harkl <3zy3@protonmail.com>
2021-09-09 13:04:33 +1200

    set user from localstore

--- src/components/Profile/EditProfile.js
+++ src/components/Profile/EditProfile.js
@@ -1,75 +1,72 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 import styled from "styled-components";
 import { Loader } from "../Loader";
 import EditProfileForm from "./EditProfileForm";
-import { USER } from "../../queries/client";
 import { PROFILE } from "../../queries/profile";
 
 const Wrapper = styled.div`
-	padding-bottom: 5rem;
+  padding-bottom: 5rem;
 
-	.flex-wrapper {
-		display: flex;
-		justify-content: center;
+  .flex-wrapper {
+    display: flex;
+    justify-content: center;
 
-		form {
-			.cover-photo {
-				margin-bottom: 1rem;
-				cursor: pointer;
-			}
+    form {
+      .cover-photo {
+        margin-bottom: 1rem;
+        cursor: pointer;
+      }
 
-			.avatar-input {
-				margin-top: -100px;
-				margin-left: 1rem;
-				cursor: pointer;
-			}
+      .avatar-input {
+        margin-top: -100px;
+        margin-left: 1rem;
+        cursor: pointer;
+      }
 
-			div.bio-wrapper {
-				background: ${props => props.theme.tertiaryColor2};
-				margin-bottom: 1.4rem;
-				border-bottom: 1px solid ${props => props.theme.accentColor};
-				padding: 0.5rem;
+      div.bio-wrapper {
+        background: ${(props) => props.theme.tertiaryColor2};
+        margin-bottom: 1.4rem;
+        border-bottom: 1px solid ${(props) => props.theme.accentColor};
+        padding: 0.5rem;
 
-				label {
-					color: ${props => props.theme.secondaryColor};
-					margin-bottom: 0.4rem;
-				}
+        label {
+          color: ${(props) => props.theme.secondaryColor};
+          margin-bottom: 0.4rem;
+        }
 
-				textarea {
-					font-size: 1rem;
-					width: 100%;
-					background: ${props => props.theme.tertiaryColor2};
-					border: none;
-					font-family: ${props => props.theme.font};
-					color: ${props => props.theme.primaryColor};
-				}
-			}
-		}
-	}
-	@media screen and (max-width: 760px) {
-		.flex-wrapper {
-			display: block;
-		}
-	}
+        textarea {
+          font-size: 1rem;
+          width: 100%;
+          background: ${(props) => props.theme.tertiaryColor2};
+          border: none;
+          font-family: ${(props) => props.theme.font};
+          color: ${(props) => props.theme.primaryColor};
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 760px) {
+    .flex-wrapper {
+      display: block;
+    }
+  }
 `;
 
 export const EditProfile = () => {
-	const {
-		data: { me }
-	} = useQuery(USER);
+  const user = localStorage.getItem("user");
 
-	const { loading, error, data } = useQuery(PROFILE, {
-		variables: { handle: me.handle }
-	});
+  const { loading, error, data } = useQuery(PROFILE, {
+    variables: { handle: user?.handle },
+  });
 
-	if (loading) return <Loader />;
+  if (loading) return <Loader />;
 
-	return (
-		<Wrapper>
-			<div className="flex-wrapper">
-				<EditProfileForm profile={data && data.profile} />
-			</div>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <div className="flex-wrapper">
+        <EditProfileForm profile={data && data.profile} />
+      </div>
+    </Wrapper>
+  );
 };
