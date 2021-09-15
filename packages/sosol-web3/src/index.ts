13c08eff7e46de533af8501ca19aba9e23df1ff0
Harkl <3zy3@protonmail.com>
2021-09-15 17:38:56 +1200

    package is executing

--- packages/sosol-web3/src/index.ts
+++ packages/sosol-web3/src/index.ts
@@ -1,4 +1,4 @@
-import anchor, { Address } from "@project-serum/anchor";
+import { Address, web3, BN, Program } from "@project-serum/anchor";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import fs from "fs";
 
@@ -9,13 +9,13 @@ export const interactionInstruction = async (
   consumerAcc: Address,
   interactionFee: any
 ) => {
-  const idl = JSON.parse(fs.readFileSync("./sosol.json", "utf8"));
-  const programId = new anchor.web3.PublicKey(
+  const idl = JSON.parse(fs.readFileSync("src/sosol.json", "utf8"));
+  const programId = new web3.PublicKey(
     "CQrhjjHtMrTnGGBSWbJaHESnUBqx7X3t9vfM6LqKqG9R"
   );
-  const program = new anchor.Program(idl, programId);
+  const program = new Program(idl, programId);
 
-  await program.rpc.interaction(new anchor.BN(interactionFee), {
+  await program.rpc.interaction(new BN(interactionFee), {
     accounts: {
       from: consumerTokenAcc,
       to: creatorTokenAcc,
