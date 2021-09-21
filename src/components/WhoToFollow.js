d6cf0ab246027a26b9841ff3b1c50328b873231f
Harkl <3zy3@protonmail.com>
2021-09-21 17:56:18 +1200

    who to follow grid scaffold

--- src/components/WhoToFollow.js
+++ src/components/WhoToFollow.js
@@ -2,44 +2,41 @@ import React from "react";
 import Avatar from "@mui/material/Avatar";
 import Follow from "./Profile/Follow";
 import PersonIcon from "@mui/icons-material/Person";
-import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { Loader } from "./Loader";
 import { USERS } from "../queries/others";
 import { useQuery } from "@apollo/client";
-import { Button } from "@mui/material";
-
-const Wrapper = styled.div`
-  margin-left: 0.4rem;
-  background: ${(props) => props.theme.tertiaryColor2};
-  border-radius: 10px;
-
-  div:last-child {
-    border-bottom: none;
-  }
-`;
+import { Button, Grid, Stack, Typography } from "@mui/material";
 
 export const User = ({ user }) => (
-  <>
-    <div className="avatar-handle">
+  <Grid container spacing={2}>
+    <Grid item xs={8}>
       <Link to={`/${user && user.handle}`}>
-        <Avatar src={user && user.avatar ? user.avatar : <PersonIcon />} />
+      <Stack direction="row">
+        <Avatar
+          sx={{ width: 24, height: 24 }}
+          src={user && user.avatar ? user.avatar : <PersonIcon />}
+        />
+        <Typography variant="body2">{user && user.fullname}</Typography>
+        </Stack>
       </Link>
-
-      <div className="handle-fullname">
-        <Link to={`/${user && user.handle}`}>
-          <span>{user && user.fullname}</span>
-        </Link>
-        <span className="secondary">@{user && user.handle}</span>
-      </div>
-    </div>
-
-    {/* {user && !user.isSelf ? (
+      <Typography variant="body2">@{user && user.handle}</Typography>
+    </Grid>
+    <Grid item xs={4}>
+      {/* {user && !user.isSelf ? (
       <Follow sm id={user && user.id} isFollowing={user && user.isFollowing} />
     ) : ( */}
-      <Button variant="contained" component={Link} to="/settings/profile">Edit Profile</Button>
-    {/* )} */}
-  </>
+      <Button
+        variant="contained"
+        component={Link}
+        to="/settings/profile"
+        size="small"
+      >
+        Edit
+      </Button>
+      {/* )} */}
+    </Grid>
+  </Grid>
 );
 
 export const WhoToFollow = () => {
@@ -49,10 +46,15 @@ export const WhoToFollow = () => {
   if (error) return <p>Error :(</p>;
 
   return (
-    <Wrapper>
+    <Stack
+      direction="column"
+      justifyContent="space-evenly"
+      alignItems="flex-start"
+      spacing={2}
+    >
       {data.users.map((user) => (
         <User key={user.id} user={user} />
       ))}
-    </Wrapper>
+    </Stack>
   );
 };
