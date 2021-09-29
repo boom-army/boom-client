7f8bc1f6c994ce28f66c8f7aa25883966a4741cc
Harkl <3zy3@protonmail.com>
2021-09-30 11:08:55 +1300

    deploy script for prod

---
+++ scripts/prod-deploy.sh
@@ -0,0 +1,10 @@
+read -p "***PRODUCTION DEPLOY*** Deploy UI to prod (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+    rm -rf ./build
+    cp .env.prod .env
+    npm run build
+    aws s3 sync ./build s3://my.sosol.app
+    cp .env.dev .env
+else
+    echo "Phew! Dodged a bullet.";
+fi
