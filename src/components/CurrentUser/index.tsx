1dc9fc79a772afd135ef03dfeee6a420881ee251
Harkl <3zy3@protonmail.com>
2021-10-01 19:41:33 +1300

    normal button all good

--- src/components/CurrentUser/index.tsx
+++ src/components/CurrentUser/index.tsx
@@ -87,7 +87,6 @@ export const CurrentUser = (props: { connected: boolean }) => {
         {connected && !token && (
           <>
             <Button
-              size="small"
               variant="contained"
               color="primary"
               onClick={handleClick}
