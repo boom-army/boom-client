28d40d5c716b93a9c6af79fe355d3812f4bc51bc
Harkl <3zy3@protonmail.com>
2021-11-03 16:31:12 +1300

    basic cards scaffold

--- src/components/Connect/ConsumerCard.js
+++ src/components/Connect/ConsumerCard.js
@@ -4,28 +4,37 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { experimentalStyled as styled } from '@mui/material/styles';
+
+const Wrapper = styled('div')(({ theme }) => ({
+  ...theme.typography.body2,
+  color: theme.palette.text.secondary,
+}));
 
 export const ConsumerCard = ({ consumer }) => {
   console.log(consumer);
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={`${window.location.origin}/default-cover.png`}
-          alt={`${consumer.handle}-${consumer.coverPhoto}`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">{consumer.consumerName}</Typography>
-          <Typography variant="body2" color="text.secondary">{consumer.bio}</Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Follow
-        </Button>
-      </CardActions>
-    </Card>
+    <Wrapper>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="80"
+            image={`${window.location.origin}/default-cover.png`}
+            alt={`${consumer.handle}-${consumer.coverPhoto}`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">{consumer.consumerName}</Typography>
+            <Typography variant="body2" color="text.secondary">{consumer.bio}</Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Follow
+          </Button>
+        </CardActions>
+      </Card>
+    </Wrapper>
   );
 };
--- src/components/Connect/Follow.js
+++ src/components/Connect/Follow.js
@@ -1,9 +1,10 @@
 import React from "react";
+import CustomResponse from "../CustomResponse";
+import Grid from "@mui/material/Grid";
+import { ConsumerCard } from "./ConsumerCard";
 import { Loader } from "../Loader";
 import { USERS } from "../../queries/follow";
 import { useQuery } from "@apollo/client";
-import { ConsumerCard } from "./ConsumerCard";
-import CustomResponse from "../CustomResponse";
 
 export const Follow = () => {
   const { loading, error, data } = useQuery(USERS);
@@ -13,9 +14,17 @@ export const Follow = () => {
   return (
     <>
       {data?.users?.length ? (
-        data.users.map((consumer) => (
-          <ConsumerCard key={consumer.id} consumer={consumer} />
-        ))
+        <Grid
+          container
+          spacing={{ md: 2 }}
+          columns={{ md: 4 }}
+        >
+          {data.users.map((consumer) => (
+            <Grid item md={4} key={`grid-${consumer.id}`}>
+              <ConsumerCard key={`card-${consumer.id}`} consumer={consumer} />
+            </Grid>
+          ))}
+        </Grid>
       ) : (
         <CustomResponse text="No other Creators to follow right now" />
       )}
