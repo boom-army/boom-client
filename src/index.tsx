48958d77883f74f9e6c1a6b11de349d6c15e47d1
Harkl <3zy3@protonmail.com>
2021-11-22 09:50:49 +1300

    sentry only in prod and test

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,18 @@ import { render } from "react-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 
-Sentry.init({
-  dsn: process.env.REACT_APP_SENTRY,
-  integrations: [new Integrations.BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY,
+    integrations: [new Integrations.BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 const RootApp = () => (
   <React.StrictMode>
