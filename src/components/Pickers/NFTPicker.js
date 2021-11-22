ca0878879f70d52796e49b3bf52f21455f3089fd
Harkl <3zy3@protonmail.com>
2021-11-22 17:19:40 +1300

    input wip

--- src/components/Pickers/NFTPicker.js
+++ src/components/Pickers/NFTPicker.js
@@ -1,50 +1,56 @@
-
 import { useEffect, useState, useContext } from "react";
-import { ReactComponent as NFTIcon } from "../../icons/nft.svg"
+import { ReactComponent as NFTIcon } from "../../icons/nft.svg";
+import { alpha, styled } from "@mui/system";
+import { InputLabel, InputBase } from "@mui/material";
+
+// TODO: integrate this with theme when whichape delivers it
+const Wrapper = styled("span")`
+  & .nft-pick {
+    margin-right: 2rem;
+    width: 26px;
+    height: 26px;
+    display: inline-block;
+    & svg {
+      width: 26px;
+      cursor: pointer;
+      & path {
+        fill: #ca2055;
+      }
+    }
+  }
+  & .MuiInputBase-input {
+    border-radius: 4px;
+    background: red;
+    padding: 10px 12px;
+    width: 100%;
+    border: 1px solid #ced4da;
+  }
+`;
 
 export const NFTPicker = () => {
-  const [picker, togglePicker] = useState(false);
+  const [nftForm, toggleNftForm] = useState(false);
 
-  const handleDocumentClick = event => {
-    let isEmojiClassFound = false;
-
-    event?.path?.forEach(elem => {
-      if (elem && elem.classList) {
-        const data = elem.classList.value;
-        if (data.includes("emoji")) {
-          isEmojiClassFound = true;
-        }
-      }
-    });
-    if (isEmojiClassFound === false && event.target.id !== "emojis-btn") togglePicker(false);
+  const handleNftSearch = (e) => {
+    console.log(e);
   };
 
-  useEffect(() => {
-    document.addEventListener("click", handleDocumentClick, false);
-    return () => {
-      document.removeEventListener("click", handleDocumentClick, false);
-    }
-  });
-
-  // const handleEmojiSelect = pickedEmoji => {
-  //   emojiHandler(pickedEmoji);
-  //   if (dismissOnClick) togglePicker(!picker);
-  // };
-
   return (
-    <>
-      <span className="nft-pick" onClick={() => togglePicker(!picker)}>
+    <Wrapper>
+      <span className="nft-pick" onClick={() => toggleNftForm(!nftForm)}>
         <NFTIcon />
       </span>
 
-      {/* {picker && (
-        <Picker
-          button={true}
-          sheetSize={64}
-          theme={theme.background === "#15202b" ? "dark" : "light"}
-          onSelect={handleEmojiSelect}
-        />
-      )}*/}
-    </> 
+      {nftForm && (
+        <>
+          <InputLabel shrink htmlFor="nft-input">
+            NFT Public Key
+          </InputLabel>
+          <InputBase
+            defaultValue="eg. 43QrHJ2csgLsRUhXW7WHQecZhRLFHW88sazGvUT65vYj"
+            id="nft-input"
+          />
+        </>
+      )}
+    </Wrapper>
   );
-}
+};
