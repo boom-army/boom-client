576d1355d1c37de53478c8604334c46dc4ec686c
Harkl <3zy3@protonmail.com>
2021-12-13 14:41:08 +1300

    tipping works

--- src/components/TipCreator/tipInput.js
+++ src/components/TipCreator/tipInput.js
@@ -27,86 +27,88 @@ export const TipInput = ({ userPubKey, setShowTip }) => {
     async ({ txAmount }) => {
       const boomTokens = txAmount * 1000000000;
       try {
-        console.log(boomTokens, txValue, txAmount);
-        if (txAmount*1 === 0) {
+        console.log(boomTokens, userPubKey);
+        if (txAmount * 1 === 0) {
           setInputError(true);
           throw new Error("You need to enter a Custom value");
         }
-        // if (!anchorWallet.publicKey) throw new WalletNotConnectedError();
-        // const sosolMint = new Token(
-        //   connection,
-        //   SOSOL_TOKEN_ID,
-        //   TOKEN_PROGRAM_ID,
-        //   anchorWallet.publicKey
-        // );
-        // const toCreatorAcc = new PublicKey(userPubKey);
-        // const associatedDestinationTokenAddr =
-        //   await Token.getAssociatedTokenAddress(
-        //     sosolMint.associatedProgramId,
-        //     sosolMint.programId,
-        //     SOSOL_TOKEN_ID,
-        //     toCreatorAcc
-        //   );
+        if (!anchorWallet.publicKey) throw new WalletNotConnectedError();
+        const sosolMint = new Token(
+          connection,
+          SOSOL_TOKEN_ID,
+          TOKEN_PROGRAM_ID,
+          anchorWallet.publicKey
+        );
+        const toCreatorAcc = new PublicKey(userPubKey);
+        const associatedDestinationTokenAddr =
+          await Token.getAssociatedTokenAddress(
+            sosolMint.associatedProgramId,
+            sosolMint.programId,
+            SOSOL_TOKEN_ID,
+            toCreatorAcc
+          );
 
-        // const receiverAccount = await connection.getAccountInfo(
-        //   associatedDestinationTokenAddr
-        // );
+        const receiverAccount = await connection.getAccountInfo(
+          associatedDestinationTokenAddr
+        );
 
-        // // TODO: move this out into a method in utils to use across the site
-        // // Create receiver sosol acc if null
-        // if (receiverAccount === null) {
-        //   const instructions = [];
-        //   instructions.push(
-        //     Token.createAssociatedTokenAccountInstruction(
-        //       sosolMint.associatedProgramId,
-        //       sosolMint.programId,
-        //       SOSOL_TOKEN_ID,
-        //       associatedDestinationTokenAddr,
-        //       toCreatorAcc,
-        //       anchorWallet.publicKey
-        //     )
-        //   );
+        // TODO: move this out into a method in utils to use across the site
+        // Create receiver sosol acc if null
+        if (receiverAccount === null) {
+          const instructions = [];
+          instructions.push(
+            Token.createAssociatedTokenAccountInstruction(
+              sosolMint.associatedProgramId,
+              sosolMint.programId,
+              SOSOL_TOKEN_ID,
+              associatedDestinationTokenAddr,
+              toCreatorAcc,
+              anchorWallet.publicKey
+            )
+          );
 
-        //   const transaction = new Transaction().add(...instructions);
-        //   transaction.feePayer = anchorWallet.publicKey;
-        //   transaction.recentBlockhash = (
-        //     await connection.getRecentBlockhash()
-        //   ).blockhash;
+          const transaction = new Transaction().add(...instructions);
+          transaction.feePayer = anchorWallet.publicKey;
+          transaction.recentBlockhash = (
+            await connection.getRecentBlockhash()
+          ).blockhash;
 
-        //   const anchorTx = await anchorWallet.signTransaction(transaction);
+          const anchorTx = await anchorWallet.signTransaction(transaction);
 
-        //   const transactionSignature = await connection.sendRawTransaction(
-        //     anchorTx.serialize(),
-        //     { skipPreflight: true }
-        //   );
+          const transactionSignature = await connection.sendRawTransaction(
+            anchorTx.serialize(),
+            { skipPreflight: true }
+          );
 
-        //   await connection.confirmTransaction(transactionSignature);
-        // }
+          await connection.confirmTransaction(transactionSignature);
+        }
 
-        // const signature = await interactionInstruction(
-        //   connection,
-        //   sosolProgram,
-        //   anchorWallet.publicKey,
-        //   new PublicKey(userPubKey),
-        //   new PublicKey(process.env.REACT_APP_CONTENT_HOST),
-        //   (boomTokens ? boomTokens : 100000000) // 0.1 SSL
-        // );
+        const signature = await interactionInstruction(
+          connection,
+          sosolProgram,
+          anchorWallet.publicKey,
+          new PublicKey(userPubKey),
+          new PublicKey(process.env.REACT_APP_CONTENT_HOST),
+          boomTokens ? boomTokens : 100000000 // 0.1 SSL
+        );
 
-        // enqueueSnackbar(`Transaction complete: ${signature}`, {
-        //   variant: "success",
-        // });
+        enqueueSnackbar(`Transaction complete: ${signature}`, {
+          variant: "success",
+        });
+        setShowTip(false);
       } catch (err) {
         console.log(err);
         return displayError(err, enqueueSnackbar);
       }
     },
     [
-      sosolProgram,
       anchorWallet,
       connection,
-      userPubKey,
       enqueueSnackbar,
+      setShowTip,
+      sosolProgram,
       txValue,
+      userPubKey,
     ]
   );
 
