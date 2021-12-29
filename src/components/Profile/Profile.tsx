362282e38e0611f6c3a43c5dc7ddb7f3702fe21b
Harkl <3zy3@protonmail.com>
2021-12-30 11:19:33 +1300

    add tabs and migrate styles to MUI

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,28 +1,17 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import styled from "styled-components";
+import React, { useContext } from "react";
 import ProfileInfo from "./ProfileInfo";
-import { ShowTweet } from "../Tweet";
+import { Box, Tab } from "@mui/material";
 import { Loader } from "../Loader";
+import { ShowTweet } from "../Tweet";
+import { TabContext, TabPanel, TabList } from "@mui/lab";
+import { ThemeContext } from "../../contexts/theme";
+import { styled } from "@mui/system";
+import { useParams } from "react-router-dom";
 import { useProfileQuery, Tweet } from "../../generated/graphql";
 
-const Wrapper = styled.div`
-	padding-bottom: 5rem;
-
-  .profile-top {
-    display: flex;
-    flex-direction: column;
-    margin-left: 1rem;
-
-    span.tweetsCount {
-      margin-top: 0.1rem;
-      color: ${(props) => props.theme.secondaryColor};
-      font-size: 0.9rem;
-    }
-  }
-`;
-
 export const Profile: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
+  const [tabValue, setTabValue] = React.useState("1");
   let { handle } = useParams<string>();
   handle = handle ? handle : "";
 
@@ -30,15 +19,54 @@ export const Profile: React.FC = () => {
     variables: { handle },
   });
 
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTabValue(newValue);
+  };
+
   if (loading) return <Loader />;
+
+  const Wrapper = styled("div")`
+    padding-bottom: 5rem;
+
+    .profile-top {
+      display: flex;
+      flex-direction: column;
+      margin-left: 1rem;
+
+      span.tweetsCount {
+        margin-top: 0.1rem;
+        color: ${theme.secondaryColor};
+        font-size: 0.9rem;
+      }
+    }
+  `;
 
   return (
     <Wrapper>
       <ProfileInfo profile={data && data.profile} />
+      <Box sx={{ width: "100%", typography: "body1" }}>
+        <TabContext value={tabValue}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Item One" value="1" />
+              <Tab label="Item Two" value="2" />
+              <Tab label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
       {data && data.profile && data.profile.tweets && data.profile.tweets.length
-        ? data.profile.tweets.map(tweet => (
-          <ShowTweet key={tweet.id} tweet={tweet as Tweet} offset={10} parentTweetId="" />
-        ))
+        ? data.profile.tweets.map((tweet) => (
+            <ShowTweet
+              key={tweet.id}
+              tweet={tweet as Tweet}
+              offset={10}
+              parentTweetId=""
+            />
+          ))
         : null}
     </Wrapper>
   );
