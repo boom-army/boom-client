0d8b885584210f288b58828b8489e22104ac2ae7
Harkl <3zy3@protonmail.com>
2022-01-04 15:11:48 +1300

    include bucket for sign-file mutation

--- src/queries/files/index.js
+++ src/queries/files/index.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const SIGN_FILE = gql`
-  mutation signFileUrl($file: String!, $type: String!) {
-    signFileUrl(file: $file, type: $type)
+  mutation signFileUrl($file: String!, $type: String!, $bucket: String) {
+    signFileUrl(file: $file, type: $type, bucket: $bucket)
   }
 `;
