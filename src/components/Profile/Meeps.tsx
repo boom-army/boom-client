00339e56684e2b3fb5679c691d9c63c112d43deb
Harkl <3zy3@protonmail.com>
2022-01-06 16:08:14 +1300

    add messaging for none meeps

--- src/components/Profile/Meeps.tsx
+++ src/components/Profile/Meeps.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 import { ShowTweet } from "../Tweet";
 import { Tweet, ProfileQuery } from "../../generated/graphql";
 
@@ -16,7 +17,11 @@ export const Meeps: React.FC<{
               parentTweetId=""
             />
           ))
-        : null}
+        : (
+          <Box pt={10} sx={{ display: "flex", justifyContent: "center" }}>
+            <Typography>You haven't Meeped yet</Typography>
+          </Box>
+        )}
     </>
   );
 };
