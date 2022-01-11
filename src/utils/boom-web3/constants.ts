80128da4d7b4bbc91c9d7bd788f066f7f6fd9a79
Harkl <3zy3@protonmail.com>
2022-01-12 10:10:18 +1300

    update boom labels

--- src/utils/boom-web3/constants.ts
+++ src/utils/boom-web3/constants.ts
@@ -1,6 +1,6 @@
 export const ERRORS = {
-  NO_SSL: "No $SSL in your account so transaction can't be actioned",
-  INSUFFICIENT_SSL: "Not enough $sosol for this transaction",
+  NO_SSL: "No $BMA in your account so transaction can't be actioned",
+  INSUFFICIENT_SSL: "Not enough $BMA for this transaction",
   MISSING_PROGRAM_PROVIDER: "Program provider missing - try again",
   NO_SSL_ACCOUNT: "No SSL account found for Payee at",
   GENERIC_CONTRACT_INTERACTION: "Error interacting with contract",
