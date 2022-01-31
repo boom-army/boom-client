f5414f3fb4f6e7f2329d9b8f87d982ffce75a917
Harkl <3zy3@protonmail.com>
2022-01-31 13:51:14 +1300

    add parallax and minting functionality

--- src/views/OGMint.tsx
+++ src/views/OGMint.tsx
@@ -10,9 +10,9 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletDialogButton } from "@solana/wallet-adapter-material-ui";
 import { useConnection } from "@solana/wallet-adapter-react";
 import {
-  awaitTransactionSignatureConfirmation,
-  CandyMachineAccount,
   CANDY_MACHINE_PROGRAM,
+  CandyMachineAccount,
+  awaitTransactionSignatureConfirmation,
   getCandyMachineState,
   mintOneToken,
 } from "../utils/candy-machine";
@@ -21,6 +21,7 @@ import { Header } from "../components/CandyMachine/Header";
 import { MintButton } from "../components/CandyMachine/MintButton";
 import { GatewayProvider } from "@civic/solana-gateway-react";
 import { Box, Grid } from "@mui/material";
+import Tilt from "react-parallax-tilt";
 
 const ConnectButton = styled(WalletDialogButton)`
   width: 100%;
@@ -32,6 +33,20 @@ const ConnectButton = styled(WalletDialogButton)`
   font-size: 16px;
   font-weight: bold;
 `;
+
+const TiltStyled = styled(Tilt)(({ theme }) => ({
+  backgroundImage: "url(/assets/outer.png)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "260px",
+  height: "260px",
+
+  transformStyle: "preserve-3d",
+}));
 
 const MintContainer = styled.div``; // add your owns styles here
 
@@ -159,63 +174,86 @@ export const OGMint = () => {
 
   return (
     <Container style={{ marginTop: 100 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={7}>
-            <Box>
-              <Paper
-                style={{
-                  padding: 24,
-                  backgroundColor: "#151A1F",
-                  borderRadius: 6,
-                }}
-              >
-                {!wallet.connected ? (
-                  <ConnectButton>Connect Wallet</ConnectButton>
-                ) : (
-                  <>
-                    <Header candyMachine={candyMachine} />
-                    <MintContainer>
-                      {candyMachine?.state.isActive &&
-                      candyMachine?.state.gatekeeper &&
-                      wallet.publicKey &&
-                      wallet.signTransaction ? (
-                        <GatewayProvider
-                          wallet={{
-                            publicKey:
-                              wallet.publicKey ||
-                              new PublicKey(CANDY_MACHINE_PROGRAM),
-                            //@ts-ignore
-                            signTransaction: wallet.signTransaction,
-                          }}
-                          gatekeeperNetwork={
-                            candyMachine?.state?.gatekeeper?.gatekeeperNetwork
-                          }
-                          clusterUrl={rpcUrl}
-                          options={{ autoShowModal: false }}
-                        >
-                          <MintButton
-                            candyMachine={candyMachine}
-                            isMinting={isUserMinting}
-                            onMint={onMint}
-                          />
-                        </GatewayProvider>
-                      ) : (
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <Box mt={4}>
+            <Paper
+              style={{
+                padding: 24,
+                backgroundColor: "#151A1F",
+                borderRadius: 6,
+              }}
+            >
+              {!wallet.connected ? (
+                <ConnectButton>Connect Wallet</ConnectButton>
+              ) : (
+                <>
+                  <Header candyMachine={candyMachine} />
+                  <MintContainer>
+                    {candyMachine?.state.isActive &&
+                    candyMachine?.state.gatekeeper &&
+                    wallet.publicKey &&
+                    wallet.signTransaction ? (
+                      <GatewayProvider
+                        wallet={{
+                          publicKey:
+                            wallet.publicKey ||
+                            new PublicKey(CANDY_MACHINE_PROGRAM),
+                          //@ts-ignore
+                          signTransaction: wallet.signTransaction,
+                        }}
+                        gatekeeperNetwork={
+                          candyMachine?.state?.gatekeeper?.gatekeeperNetwork
+                        }
+                        clusterUrl={rpcUrl}
+                        options={{ autoShowModal: false }}
+                      >
                         <MintButton
                           candyMachine={candyMachine}
                           isMinting={isUserMinting}
                           onMint={onMint}
                         />
-                      )}
-                    </MintContainer>
-                  </>
-                )}
-              </Paper>
-            </Box>
-          </Grid>
-          <Grid item xs={5}>
-            Boom
-          </Grid>
+                      </GatewayProvider>
+                    ) : (
+                      <MintButton
+                        candyMachine={candyMachine}
+                        isMinting={isUserMinting}
+                        onMint={onMint}
+                      />
+                    )}
+                  </MintContainer>
+                </>
+              )}
+            </Paper>
+          </Box>
         </Grid>
+        <Grid item xs={5}>
+          <TiltStyled
+            className="track-on-window"
+            tiltMaxAngleX={30}
+            tiltMaxAngleY={30}
+            perspective={800}
+            transitionSpeed={1500}
+            trackOnWindow={true}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                transform: "translateZ(30px)",
+              }}
+            >
+              <img
+                src={"/assets/inner.png"}
+                width="120"
+                className="inner-element"
+                alt="1303 OG NFT Card"
+              />
+            </Box>
+          </TiltStyled>
+        </Grid>
+      </Grid>
 
       <Snackbar
         open={alertState.open}
