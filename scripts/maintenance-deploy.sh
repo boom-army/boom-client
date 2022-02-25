854f305ea10b57e2c023ff8c80045088cfec8d4d
Harkl <3zy3@protonmail.com>
2022-02-25 15:58:42 +1300

    add maintenance mode screen

---
+++ scripts/maintenance-deploy.sh
@@ -0,0 +1,10 @@
+read -p "***MAINTENANCE DEPLOY*** Deploy UI to prod (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+    rm -rf ./build
+    mkdir ./build
+    cp ./maintenance.html ./build/index.html
+    aws s3 rm s3://app.boom.army --recursive
+    aws s3 sync ./build s3://app.boom.army
+else
+    echo "Phew! No maintenance today.";
+fi
