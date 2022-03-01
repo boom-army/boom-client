d66942773216acdaa4746955e8d5e5d6c106ce01
Harkl <3zy3@protonmail.com>
2022-03-01 15:02:59 +1300

    style boxes for creators

--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import { CustomResponse } from "../CustomResponse";
 import Grid from "@mui/material/Grid";
 import { ConsumerCard } from "./ConsumerCard";
@@ -6,11 +6,22 @@ import { Loader } from "../Loader";
 import { USERS } from "../../queries/follow";
 import { useQuery } from "@apollo/client";
 import { Box } from "@mui/system";
+import { styled } from "@mui/material/styles";
+import { ThemeContext } from "../../contexts/theme";
+
 
 export const Connect = () => {
+  const { theme } = useContext(ThemeContext);
   const { loading, data, fetchMore } = useQuery(USERS, {
     variables: {
       offset: 0,
+    },
+  });
+
+  const StyledGrid = styled(Grid)({
+    "@media (max-width: 600px)": {
+      borderBottom: `1px solid ${theme.secondaryColor}`,
+      marginBottom: "1em",
     },
   });
 
@@ -47,9 +58,9 @@ export const Connect = () => {
           sx={{ paddingTop: 3, paddingLeft: 2, paddingRight: 2 }}
         >
           {data.users.map((consumer) => (
-            <Grid item md={4} key={`grid-${consumer.id}`}>
+            <StyledGrid item xs={12} md={4} key={`grid-${consumer.id}`}>
               <ConsumerCard key={`card-${consumer.id}`} consumer={consumer} />
-            </Grid>
+            </StyledGrid>
           ))}
         </Grid>
       ) : (
