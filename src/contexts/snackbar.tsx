76c970d6630d304f90a29dd71cb884cf627acc53
Harkl <3zy3@protonmail.com>
2022-05-09 15:24:18 +1200

    set theme for snackbar

--- src/contexts/snackbar.tsx
+++ src/contexts/snackbar.tsx
@@ -1,6 +1,7 @@
-import React, { createContext, useContext } from 'react';
-import { Alert, AlertColor } from '@mui/material';
-import { Snackbar } from '@material-ui/core';
+import React, { createContext, useContext } from "react";
+import { Alert, AlertColor } from "@mui/material";
+import { ThemeContext } from "./theme";
+import { Snackbar } from "@material-ui/core";
 
 type SnackBarContextActions = {
   enqueueSnackbar: (text: string, { variant }: { variant: AlertColor }) => void;
@@ -15,11 +16,16 @@ interface SnackBarContextProviderProps {
 const SnackbarProvider: React.FC<SnackBarContextProviderProps> = ({
   children,
 }) => {
-  const [open, setOpen] = React.useState<boolean>(false);
-  const [message, setMessage] = React.useState<string>('');
-  const [typeColor, setTypeColor] = React.useState<AlertColor>('info');
+  const { theme } = useContext(ThemeContext);
 
-  const enqueueSnackbar = (text: string, { variant }: { variant: AlertColor }) => {
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [message, setMessage] = React.useState<string>("");
+  const [typeColor, setTypeColor] = React.useState<AlertColor>("info");
+
+  const enqueueSnackbar = (
+    text: string,
+    { variant }: { variant: AlertColor }
+  ) => {
     setMessage(text);
     setTypeColor(variant);
     setOpen(true);
@@ -27,17 +33,22 @@ const SnackbarProvider: React.FC<SnackBarContextProviderProps> = ({
 
   const handleClose = () => {
     setOpen(false);
-    setTypeColor('info');
+    setTypeColor("info");
   };
 
   return (
     <SnackBarContext.Provider value={{ enqueueSnackbar }}>
       <Snackbar
         open={open}
-        autoHideDuration={6000}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        onClose={handleClose}>
-        <Alert onClose={handleClose} severity={typeColor}>
+        autoHideDuration={100000}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={typeColor}
+          sx={{ width: "100%", border: `1px solid ${theme.tertiaryColor}` }}
+        >
           {message}
         </Alert>
       </Snackbar>
@@ -50,7 +61,7 @@ const useSnackbar = (): SnackBarContextActions => {
   const context = useContext(SnackBarContext);
 
   if (!context) {
-    throw new Error('useSnackbar must be used within the SnackbarProvider');
+    throw new Error("useSnackbar must be used within the SnackbarProvider");
   }
 
   return context;
