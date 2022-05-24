f02b9565622489510215f6eb10875622e6c23528
Harkl <3zy3@protonmail.com>
2022-05-25 11:53:43 +1200

    add video playback in tl

--- src/components/UrlMeta/UrlMetaData.tsx
+++ src/components/UrlMeta/UrlMetaData.tsx
@@ -22,6 +22,9 @@ export const UrlMetaData: React.FC<Props> = ({ url }: Props) => {
   const images = data?.getMeta?.open_graph?.images ||
     data?.getMeta?.twitter_card?.images || [{ url: null }];
   const heroImage = images[0]?.url;
+  const videos = data?.getMeta?.open_graph?.videos ||
+    data?.getMeta?.twitter_card?.players || [{ url: null }];
+  const heroVideo = videos[0]?.url;
   const rootUrl = url?.match(
     /[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*(\.[A-Za-z]{2,})/g
   );
@@ -31,13 +34,21 @@ export const UrlMetaData: React.FC<Props> = ({ url }: Props) => {
 
   return hasData ? (
     <Card sx={{ maxWidth: 400, margin: "1em 0" }}>
-      <CardActionArea onClick={() => window.open(url, '_blank')?.focus()}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={heroImage}
-          alt={data?.getMeta?.title || ""}
-        />
+      <CardActionArea onClick={() => window.open(url, "_blank")?.focus()}>
+        {heroVideo ? (
+          <CardMedia
+            component="iframe"
+            image={heroVideo}
+            height="240"
+          />
+        ) : (
+          <CardMedia
+            component="img"
+            height="140"
+            image={heroImage}
+            alt={data?.getMeta?.title || ""}
+          />
+        )}
         <CardContent>
           <Typography gutterBottom variant="body2" component="div">
             {rootUrl && rootUrl[0]}
