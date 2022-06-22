377612c8834590b4f47e06029462dfa5adcfb325
Harkl <3zy3@protonmail.com>
2022-06-22 17:41:26 +1200

    add card action to news

--- src/components/NewsItem/index.tsx
+++ src/components/NewsItem/index.tsx
@@ -3,9 +3,10 @@ import ModeCommentIcon from "@mui/icons-material/ModeComment";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import stc from "string-to-color";
-import { styled, Box, Typography } from "@mui/material";
-import { SearchTweetsQuery } from "../../generated/graphql";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { Link as RouterLink } from "react-router-dom";
+import { SearchTweetsQuery } from "../../generated/graphql";
+import { styled, Box, Typography, Card, CardActionArea } from "@mui/material";
 
 const IconBox = styled(Box)((props: any) => ({
   color: props.theme.blue.light,
@@ -31,49 +32,56 @@ interface NewsProps {
 export const NewsItem = ({ meep }: NewsProps) => {
   const [text] = meep.text.split("#");
   return (
-    <Box p={1}>
-      <Typography
-        display="inline"
-        mr={1}
-        sx={{ color: stc(meep.user?.handle) }}
+    <Card>
+      <CardActionArea
+        component={RouterLink}
+        to={`/${meep.user?.handle}/status/${meep.id}`}
       >
-        {meep.user?.handle} /
-      </Typography>
-      <Typography display="inline">{text}</Typography>
-      <Typography display="inline">
-        {meep.reactions?.length ? (
-          <IconBox display="inline-block">
-            <SentimentSatisfiedAltIcon />{" "}
-            <IconTypography display="inline">
-              {meep.reactions?.length}
-            </IconTypography>
-          </IconBox>
-        ) : null}
-        {parseInt(meep.tipsCount as string) > 0 && (
-          <IconBox display="inline-block">
-            <MonetizationOnIcon />{" "}
-            <IconTypography display="inline">
-              {parseInt(meep.tipsCount as string) / LAMPORTS_PER_SOL}
-            </IconTypography>
-          </IconBox>
-        )}
-        {meep.commentsCount ? (
-          <IconBox display="inline-block">
-            <ModeCommentIcon />{" "}
-            <IconTypography display="inline">
-              {meep.commentsCount}
-            </IconTypography>
-          </IconBox>
-        ) : null}
-        {meep.retweetsCount ? (
-          <IconBox display="inline-block">
-            <LoopIcon />{" "}
-            <IconTypography display="inline">
-              {meep.retweetsCount}
-            </IconTypography>
-          </IconBox>
-        ) : null}
-      </Typography>
-    </Box>
+        <Box p={1}>
+          <Typography
+            display="inline"
+            mr={1}
+            sx={{ color: stc(meep.user?.handle) }}
+          >
+            {meep.user?.handle} /
+          </Typography>
+          <Typography display="inline">{text}</Typography>
+          <Typography display="inline">
+            {meep.reactions?.length ? (
+              <IconBox display="inline-block">
+                <SentimentSatisfiedAltIcon />{" "}
+                <IconTypography display="inline">
+                  {meep.reactions?.length}
+                </IconTypography>
+              </IconBox>
+            ) : null}
+            {parseInt(meep.tipsCount as string) > 0 && (
+              <IconBox display="inline-block">
+                <MonetizationOnIcon />{" "}
+                <IconTypography display="inline">
+                  {parseInt(meep.tipsCount as string) / LAMPORTS_PER_SOL}
+                </IconTypography>
+              </IconBox>
+            )}
+            {meep.commentsCount ? (
+              <IconBox display="inline-block">
+                <ModeCommentIcon />{" "}
+                <IconTypography display="inline">
+                  {meep.commentsCount}
+                </IconTypography>
+              </IconBox>
+            ) : null}
+            {meep.retweetsCount ? (
+              <IconBox display="inline-block">
+                <LoopIcon />{" "}
+                <IconTypography display="inline">
+                  {meep.retweetsCount}
+                </IconTypography>
+              </IconBox>
+            ) : null}
+          </Typography>
+        </Box>
+      </CardActionArea>
+    </Card>
   );
 };
