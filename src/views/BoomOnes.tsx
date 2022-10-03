a03c995f02bb3d5cd9ad489da4ed80ba7c302653
harkl <3zy3@protonmail.ch>
2022-10-03 17:36:23 +1300

    scaffold grid

--- src/views/BoomOnes.tsx
+++ src/views/BoomOnes.tsx
@@ -1,5 +1,37 @@
+import { Box, Grid } from "@mui/material";
+
 export const BoomOnes = () => {
-    return (
-        <>BoomOnes</>
-    )
-}
+  return (
+    <Grid container mt={2} px={2}>
+      <Grid item xs={12}>
+        <Box>
+            Harkl leads!
+            Share button 
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Box>
+          <img
+            src={"/assets/420.png"}
+            width="200"
+            alt="Boom Heroes #420"
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        Name
+        Family
+        Description 
+        Bid History 
+        Creator 
+        Series
+        Details
+        Metadata 
+        Price
+      </Grid>
+      <Grid item xs={12}>
+        Promo box 
+      </Grid>
+    </Grid>
+  );
+};
