3b60be89b4398db8c60b11bd5434d94bd2f7265d
harkl <3zy3@protonmail.ch>
2023-03-03 16:02:38 +1300

    sorting cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,49 @@
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import viteTsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  esbuild: {
-    target: "esnext",
-  },
-  build: {
-    sourcemap: false,
-    minify: "esbuild",
-    outDir: "build",
-    rollupOptions: {
-      cache: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  process.env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    esbuild: {
+      target: "esnext",
     },
-  },
-  plugins: [
-    react(),
-    viteTsconfigPaths(),
-    svgr(),
-    nodePolyfills({
-      protocolImports: true,
-    }),
-    sentryVitePlugin({
-      include: ".",
-      ignore: ["node_modules", "vite.config.ts"],
-      org: "sosol",
-      project: "react-client",
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  server: {
-    port: 3000,
-    cors: {
-      origin: "*",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    build: {
+      sourcemap: false,
+      minify: "esbuild",
+      outDir: "build",
+      rollupOptions: {
+        cache: false,
+      },
     },
-  },
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      svgr(),
+      nodePolyfills({
+        protocolImports: true,
+      }),
+      sentryVitePlugin({
+        include: ".",
+        ignore: ["node_modules", "vite.config.ts"],
+        org: "sosol",
+        project: "react-client",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    server: {
+      port: 3000,
+      cors: {
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      },
+    },
+  };
 });
