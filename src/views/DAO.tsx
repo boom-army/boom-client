9a31e3769ddbcbb1fa8cdd9d280d4a5e737d62b8
harkl <3zy3@protonmail.ch>
2023-05-15 10:50:25 +1200

    scaffold dao promo page

--- src/views/DAO.tsx
+++ src/views/DAO.tsx
@@ -1,12 +1,48 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 export const DAOView = () => {
+  const handleSignUp = () => {};
+
   return (
-    <Grid container>
-        <Grid item xs={12} md={12} lg={12}>
-            <h1>DAO</h1>
-        </Grid>
+    <Grid container rowSpacing={2} sx={{ ml: 2, mr: 2 }}>
+      <Grid item xs={12}>
+        <Box>
+          <Typography variant="h2">
+            Have we really been doing DAO's on Solana?
+          </Typography>
+          <img src="promotional-image.jpg" alt="Promotional Image" />
+          <Typography variant="body1">
+            Boom is about to solve NFT-based DAO solutions. Aren't you sick of
+            third-party add-ons and integrations to make your DAO work? Boom is
+            building the ULTIMATE solution for turn-key decentralized governance
+            and community collaboration. No more hollering at your homies for
+            their Telegram deets. No more trying to figure out how to get your
+            DAO to work with your NFTs. Boom is here to make it all work
+            seamlessly.
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={handleSignUp}>
+          Join the Waiting List
+        </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Buy a Boom Hero and be part of the alpha crew with full access and participation in the build</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Shit post on Meeper while you wait</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Read up on how our dev is progressing</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>Get some $BMA and beat the liquidity squeeze</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>VCs get in touch to be part of the funding round</Typography>
+      </Grid>
     </Grid>
   );
 };
