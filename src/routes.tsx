59401337c0c03994e15c83f712f2529dd6a5b280
harkl <3zy3@protonmail.ch>
2023-05-16 08:46:03 +1200

    ignore helmet

--- src/routes.tsx
+++ src/routes.tsx
@@ -78,6 +78,7 @@ export const AppRoutes: React.FC = () => {
 
   return (
     <>
+      {/* @ts-ignore */}
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
