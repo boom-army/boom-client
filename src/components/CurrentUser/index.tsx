4d0ccb1c2d2e1f3201dc63c2ebfafa1cb07ef35a
harkl <3zy3@protonmail.ch>
2023-05-16 09:21:44 +1200

    add notification icon to the top right context panel

--- src/components/CurrentUser/index.tsx
+++ src/components/CurrentUser/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Grid, IconButton } from "@mui/material";
+import { Badge, Box, Grid, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useToggleDrawer } from "../../hooks";
 import { WalletMultiButton } from "../WalletMUIConnect";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 export const CurrentUser = () => {
   const toggleDrawer = useToggleDrawer();
@@ -16,6 +17,11 @@ export const CurrentUser = () => {
       >
         <Box mr={1} display="flex" alignItems="center">
           <WalletMultiButton />
+          <IconButton aria-label="notifications">
+            <Badge badgeContent={1} color="primary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
           <IconButton
             color="primary"
             onClick={toggleDrawer(true)}
