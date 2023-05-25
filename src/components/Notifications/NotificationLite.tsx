c6c0e99703f5f7e394ca8e14532348f9bf181d31
harkl <3zy3@protonmail.ch>
2023-05-25 12:06:37 +1200

    Retool notification format to make it more intuitive and prepare for channel notifications

--- src/components/Notifications/NotificationLite.tsx
+++ src/components/Notifications/NotificationLite.tsx
@@ -1,15 +1,15 @@
-import { Box, Typography, useTheme } from "@mui/material";
+import { Box, Stack, Typography, useTheme, alpha } from "@mui/material";
 import { Emoji } from "emoji-mart";
 import { HARKL_ID } from "../../utils/utils";
 import { HerofiedIcon } from "../Icons";
 import { Link } from "react-router-dom";
+import ReplyIcon from "@mui/icons-material/Reply";
 import { Mention, useProfileByIdQuery, User } from "../../generated/graphql";
+import { MentionTypes, RoutePath } from "../../constants";
+import { ShowTweet } from "../Tweet";
+import { ThreadReply } from "../Tweet/TweetThread/ThreadReply";
 import { UserAvatar } from "../UserAvatar";
 import { useEffect, useState } from "react";
-import moment from "moment";
-import { setDate } from "../../utils";
-import { ShowTweet } from "../Tweet";
-import { RoutePath } from "../../constants";
 import { useNewMentions } from "../../hooks";
 
 interface NotificationProps {
@@ -22,6 +22,7 @@ export const NotificationLite = ({ mention }: NotificationProps) => {
   const [fromUser, setFromUser] = useState(mention?.tweet?.user);
   const { newMentions } = useNewMentions();
 
+  const parentTweet = { ...mention.tweet?.parentTweet, user: fromUser };
   const mentionIsNew =
     newMentions?.length &&
     newMentions?.some((newMention) => newMention.id === mention.id);
@@ -37,11 +38,11 @@ export const NotificationLite = ({ mention }: NotificationProps) => {
 
   useEffect(() => {
     switch (mention.type) {
-      case "mention":
-        setText("mentioned you in a meep");
+      case MentionTypes.MENTION:
+        setText("Mention in");
         break;
-      case "reply":
-        setText("replied to your meep");
+      case MentionTypes.REPLY:
+        setText("Replied in");
         break;
       default:
         break;
@@ -49,83 +50,77 @@ export const NotificationLite = ({ mention }: NotificationProps) => {
   }, [mention]);
 
   return (
-    <Box p={2} sx={{ borderBottom: `1px solid ${theme.tertiaryColor}` }}>
-      {mentionIsNew && ("Boom")}
-      {fromUser && (
-        <Box
-          display={"flex"}
-          sx={{ flexWrap: "wrap", wordBreak: "break-word" }}
-        >
-          <Box mr={0.5} position="relative">
-            <Link to={`${RoutePath.HANDLE_HASH}/${fromUser.handle}`}>
-              <UserAvatar
-                className="avatar"
-                avatar={fromUser?.avatar as string}
-                handle={fromUser?.handle}
-                isNFT={fromUser?.data?.avatarMint}
-                sx={{
-                  width: "1.1rem",
-                  height: "1.1rem",
-                  mt: "0.1rem",
-                }}
-              />
-            </Link>
-          </Box>
-          <Typography
-            variant="body2"
-            color="secondary"
-            sx={{ fontWeight: "600", mr: 0.5 }}
-          >
-            {fromUser.consumerName}
-          </Typography>
-          <Typography
-            mr={0.5}
-            variant="body2"
-            color="secondary"
-          >{`@${fromUser.handle}`}</Typography>
-          {fromUser?.data?.avatarUpdateAuthority === HARKL_ID && (
-            <HerofiedIcon
-              sx={{
-                fill: theme.accentColor,
-                width: "0.8rem",
-                height: "0.8rem",
-                verticalAlign: "1px",
-              }}
-            />
-          )}
-          {mention.type && mention.type.includes("emoji") ? (
-            <Box>
-              <Typography variant="body2" color="secondary" pl={0.5}>
-                reacted{" "}
-                <Box
-                  display={"inline"}
-                  sx={{
-                    "& .emoji-mart-emoji": { verticalAlign: "-3px" },
-                  }}
-                >
-                  {mention?.common?.emojiId && (
-                    <Emoji emoji={mention?.common?.emojiId} size={14} />
-                  )}
-                </Box>
-              </Typography>
-            </Box>
-          ) : (
-            <Typography variant="body2" color="secondary" pl={0.5}>
-              {text}
+    <Stack
+      direction="column"
+      p={2}
+      sx={{
+        borderBottom: `1px solid ${theme.tertiaryColor}`,
+        backgroundColor: mentionIsNew
+          ? alpha(theme.accentColor, 0.1)
+          : "transparent",
+      }}
+    >
+      {mention.type === MentionTypes.REPLY && (
+        <Box>
+          <Box display="flex" alignItems="center">
+            <ReplyIcon sx={{ color: theme.accentColor }} fontSize="small" />
+            <Typography variant="body2" color="secondary">
+              {text}{" "}
+              {mention.tweet?.channel ? `${mention.tweet?.channel}` : "Feed"}
             </Typography>
+          </Box>
+          <Box pb={1.5}>
+            {/* @ts-ignore */}
+            <ThreadReply tweet={parentTweet} />
+          </Box>
+        </Box>
+      )}
+      {mention.type === MentionTypes.EMOJI && (
+        <Box display="flex">
+          {mention?.common?.emojiId && (
+            <Box mr={1}>
+              <Emoji emoji={mention?.common?.emojiId} size={20} />
+            </Box>
           )}
-          <Typography variant="body2" color="secondary" pl={0.5}>
-            {moment(setDate(mention.createdAt)).fromNow()}
-          </Typography>
+          {fromUser && (
+            <Box display={"flex"}>
+              <Box mr={0.5} position="relative">
+                <Link to={`${RoutePath.HANDLE_HASH}/${fromUser.handle}`}>
+                  <UserAvatar
+                    className="avatar"
+                    avatar={fromUser?.avatar as string}
+                    handle={fromUser?.handle}
+                    isNFT={fromUser?.data?.avatarMint}
+                    sx={{
+                      width: "1.1rem",
+                      height: "1.1rem",
+                      mt: "0.1rem",
+                    }}
+                  />
+                </Link>
+              </Box>
+              <Typography
+                mr={0.5}
+                variant="body2"
+                color="secondary"
+              >{`@${fromUser.handle}`}</Typography>
+              {fromUser?.data?.avatarUpdateAuthority === HARKL_ID && (
+                <HerofiedIcon
+                  sx={{
+                    fill: theme.accentColor,
+                    width: "0.8rem",
+                    height: "0.8rem",
+                    verticalAlign: "1px",
+                  }}
+                />
+              )}
+            </Box>
+          )}
         </Box>
       )}
       {mention.tweet && (
-        <ShowTweet
-          key={mention.id}
-          tweet={mention.tweet}
-          overideMt={0.5}
-        ></ShowTweet>
+        <ShowTweet key={mention.id} tweet={mention.tweet} overideMt={0.5} />
       )}
-    </Box>
+    </Stack>
   );
 };
