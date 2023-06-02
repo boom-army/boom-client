d7a7741fe6af84914400b32d2ed29ddddf31ca35
harkl <3zy3@protonmail.ch>
2023-06-02 14:54:39 +1200

    Load DAOs from users who have wallets connected

--- src/views/DAO.tsx
+++ src/views/DAO.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import BuildIcon from "@mui/icons-material/Build";
@@ -7,7 +7,6 @@ import LockOpenIcon from "@mui/icons-material/LockOpen";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import {
   Box,
-  Button,
   Grid,
   List,
   ListItem,
@@ -19,11 +18,128 @@ import {
 } from "@mui/material";
 import { Fxnction } from "../components/DAOTweets/Fxnction";
 import { DAOPromo } from "../components/Advertising/DAOPromo";
-import { RoutePath } from "../constants";
+import { ChannelStatus, RoutePath } from "../constants";
+import { publicKey } from "@metaplex-foundation/beet-solana";
+import { uniqBy, differenceBy } from "lodash";
+import { client } from "../apollo/client";
+import { displayError } from "../utils";
+import { useApolloClient } from "@apollo/client";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useMetaplex } from "../contexts/metaplex";
+import { useSnackbar } from "../contexts/snackbar";
+import {
+  useGetUserChannelsLazyQuery,
+  useAddChannelMutation,
+  useUnlinkChannelMutation,
+} from "../generated/graphql";
 
 export const DAOView = () => {
-  const handleSignUp = () => {};
   const theme = useTheme();
+  const { connection } = useConnection();
+  const metaplex = useMetaplex();
+  const client = useApolloClient();
+  const { publicKey } = useWallet();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [getUserChannels, { data, loading, error }] =
+    useGetUserChannelsLazyQuery({
+      fetchPolicy: "network-only",
+    });
+  const [addChannelMutation] = useAddChannelMutation();
+  const [channelUnlinkMutation] = useUnlinkChannelMutation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (!publicKey) displayError("Wallet not connected", enqueueSnackbar);
+        const nftData = publicKey
+          ? await metaplex?.nfts().findAllByOwner({
+              owner: publicKey,
+            })
+          : [];
+
+        const formatChannelData =
+          nftData?.map(async (data, i) => {
+            console.log(data);
+            try {
+              const meta: any = await fetch(data.uri).then((response) =>
+                response.json()
+              );
+              return {
+                __typename: "Channel" as "Channel",
+                // @ts-ignore
+                id: data.mintAddress.toBase58() ?? "",
+                mintAuthority: data.updateAuthorityAddress.toBase58() ?? "",
+                collection: data.collection?.address ?? "",
+                name: meta?.collection?.name ?? "",
+                family: meta?.collection?.family ?? "",
+                status: "active",
+                image: meta.image ?? "",
+                description: meta.description ?? "",
+                verified: null,
+                channelParentId: null,
+                membersCount: null,
+              };
+            } catch (error) {
+              return Promise.resolve({
+                name: "",
+                family: "",
+                mintAuthority: "",
+                description: "",
+                image: "",
+                status: "",
+              });
+            }
+          }) || [];
+        const channelData = await Promise.all(formatChannelData);
+        const validChannels = channelData?.filter(
+          (channel) => channel?.name || channel?.family
+        );
+        const uniqueChannels = uniqBy(validChannels, (d) =>
+          [d?.mintAuthority, d?.name, d?.family].join()
+        );
+        const currentChannels = await getUserChannels();
+        const channelDiff = differenceBy(
+          currentChannels?.data?.getUserChannels || [],
+          uniqueChannels,
+          (d) => [d?.mintAuthority, d?.name, d?.family].join()
+        );
+        // Unlink stale NFT channels
+        if (channelDiff.length) {
+          const unlinkStaleNFTs = channelDiff.map(async (channel) => {
+            try {
+              await channelUnlinkMutation({
+                variables: { channelId: channel.id },
+              });
+            } catch (error) {
+              return Promise.resolve();
+            }
+          });
+          await Promise.all(unlinkStaleNFTs);
+        }
+        if (uniqueChannels.length) {
+          const unlinkStaleNFTs = uniqueChannels.map(async (channel) => {
+            try {
+              await addChannelMutation({
+                variables: {
+                  mintAuthority: channel.mintAuthority,
+                  name: channel.name,
+                  family: channel.family,
+                  description: channel.description,
+                  image: channel.image,
+                  status: ChannelStatus.ACTIVE,
+                  channelParentId: null,
+                },
+              });
+            } catch (error) {
+              return Promise.resolve();
+            }
+          });
+          await Promise.all(unlinkStaleNFTs);
+        }
+      } catch (error) {}
+    })();
+  }, [connection, publicKey, client]);
 
   return (
     <Grid container justifyContent="center" mt={2} px={2}>
